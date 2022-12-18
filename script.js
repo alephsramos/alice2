8f509145bf594e7c63e9074ebcd0be57bf13506f
aleph ramos <105456824+alephsramos@users.noreply.github.com>
2022-12-17 21:52:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function enviar(){
   dv = document.getElementById("dvResultado");
 
   dv.innerHTML = "<br><h1>⋘ Relatório ⋙</h1><br>" + 
-  "‣ Nome: <b> " + nome + "</b><br>" +
+  "‣ Nome: <b>" + nome + "</b><br>" +
   "‣ Sexo: <b>" + opSexo + "</b><br>" +
   "‣ Idade: <b>" + idade + "</b><br>";
 }
